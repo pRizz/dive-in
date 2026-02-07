06640cd0a8d4c8096216fe3c1eae1b63f7af4676
Peter Ryszkiewicz <pRizz@users.noreply.github.com>
2026-02-07 10:23:34 -0600

    fix(ui): add spacing for efficiency ring label

--- ui/src/ring.tsx
+++ ui/src/ring.tsx
@@ -6,30 +6,33 @@ import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 
 export default function CircularProgressWithLabel(
-    props: CircularProgressProps & { value: number },
-  ) {
-    return (
-      <Box sx={{ position: 'relative', display: 'inline-flex' }}>
-        <CircularProgress size="4rem" variant="determinate" {...props} />
-        <Box
+  props: CircularProgressProps & { value: number },
+) {
+  return (
+    <Box sx={{ position: 'relative', display: 'inline-flex' }}>
+      <CircularProgress size="4rem" variant="determinate" {...props} />
+      <Box
+        sx={{
+          top: 0,
+          left: 0,
+          bottom: 0,
+          right: 0,
+          position: 'absolute',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}
+      >
+        <Typography
+          component="div"
+          color="text.secondary"
           sx={{
-            top: 0,
-            left: 0,
-            bottom: 0,
-            right: 0,
-            position: 'absolute',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
+            fontSize: '1.5rem',
+            lineHeight: 1,
+            textAlign: 'center',
           }}
-        >
-          <Typography
-            variant="h3"
-            component="div"
-            color="text.secondary"
-          >{`${Math.round(props.value)}%`}</Typography>
-        </Box>
+        >{`${Math.round(props.value)}%`}</Typography>
       </Box>
-    );
-  }
-  
+    </Box>
+  );
+}
